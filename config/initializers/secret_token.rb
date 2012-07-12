e8df1b52aba4d2622a3ac31f6d65f34606cd2286
railsuca <railsuca@yopmail.com>
2012-07-12 14:13:09 -0600

    Primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitrails::Application.config.secret_token = '83816f72676953183d6edff864f8911c3375cbb2bdbcfb024c5d5f263b462433f5acac0cb72fe71b1fa217c32c808fe0fe4b423a221e52f7091f46ce51370843'
